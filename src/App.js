80976fd41863f29ce63fb3f5d97adbee6ff91844
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-02-17 18:32:38 -0500

    feat(fake-store): creating a product page which has image, titil and price as props and the products despaly in a list or grid.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Nav from './Components/Nav/Nav';
+import ProductsPage from './Components/product/ProductsPage';
 
 function App() {
   return (
     <div className="App">
           <Nav />
+          <ProductsPage/>
     </div>
   );
 }
